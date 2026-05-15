add4e6f8f4c70886bad4bc03e0c1f76053d8e2f5
JJ <warmachinesocial@skiff.com>
2026-05-15 16:23:28 -0500

    Fix - removed revenuecat

--- app/src/main/java/com/wherefam/android/MainApplication.kt
+++ app/src/main/java/com/wherefam/android/MainApplication.kt
@@ -1,9 +1,6 @@
 package com.wherefam.android
 
 import android.app.Application
-import com.revenuecat.purchases.LogLevel
-import com.revenuecat.purchases.Purchases
-import com.revenuecat.purchases.PurchasesConfiguration
 import com.wherefam.android.di.appModule
 import com.wherefam.android.di.viewModel
 import org.koin.android.ext.koin.androidContext
@@ -16,14 +13,5 @@ class MainApplication : Application() {
             androidContext(this@MainApplication)
             modules(appModule, viewModel)
         }
-
-
-        Purchases.logLevel = LogLevel.DEBUG
-        Purchases.configure(
-            PurchasesConfiguration.Builder(
-                context = this,
-                apiKey = ""
-            ).build()
-        )
     }
 }
--- app/src/main/java/com/wherefam/android/core/root/ContentView.kt
+++ app/src/main/java/com/wherefam/android/core/root/ContentView.kt
@@ -5,11 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.revenuecat.purchases.CustomerInfo
-import com.revenuecat.purchases.models.StoreTransaction
-import com.revenuecat.purchases.ui.revenuecatui.PaywallDialog
-import com.revenuecat.purchases.ui.revenuecatui.PaywallDialogOptions
-import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 import com.wherefam.android.core.home.HomeView
 import com.wherefam.android.core.onboarding.*
 
@@ -32,30 +27,6 @@ fun ContentView(navController: NavHostController, startDestination: String) {
 
         composable("Home") {
             HomeView()
-        }
-
-        composable("Paywall") {
-            PaywallDialog(
-                paywallDialogOptions = PaywallDialogOptions.Builder()
-                    .setDismissRequest {
-                        navController.popBackStack()
-                    }
-                    .setListener(
-                        object : PaywallListener {
-                            override fun onPurchaseCompleted(
-                                customerInfo: CustomerInfo,
-                                storeTransaction: StoreTransaction
-                            ) {
-                                super.onPurchaseCompleted(customerInfo, storeTransaction)
-                            }
-
-                            override fun onRestoreCompleted(customerInfo: CustomerInfo) {
-                                super.onRestoreCompleted(customerInfo)
-                            }
-                        }
-                    )
-                    .build()
-            )
         }
     }
 }
