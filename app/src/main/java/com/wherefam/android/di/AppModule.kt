57054ef3f6053cd08b9687de8a38ff19ce1cfed5
JJ <warmachinesocial@skiff.com>
2026-05-16 10:46:48 -0500

    Fix - Updated Koin DI module

--- app/src/main/java/com/wherefam/android/di/AppModule.kt
+++ app/src/main/java/com/wherefam/android/di/AppModule.kt
@@ -3,10 +3,15 @@ package com.wherefam.android.di
 import androidx.room.Room
 import com.wherefam.android.core.home.HomeViewModel
 import com.wherefam.android.core.home.people.PeopleViewModel
+import com.wherefam.android.core.home.places.PlacesViewModel
+import com.wherefam.android.core.home.safety.SafetyViewModel
 import com.wherefam.android.core.home.share.ShareViewModel
+import com.wherefam.android.core.home.settings.SettingsViewModel
 import com.wherefam.android.core.onboarding.OnboardingViewModel
 import com.wherefam.android.core.onboarding.SplashViewModel
 import com.wherefam.android.core.onboarding.ThirdPageViewModel
+import com.wherefam.android.data.MIGRATION_1_2
+import com.wherefam.android.data.MIGRATION_2_3
 import com.wherefam.android.data.UserRepository
 import com.wherefam.android.data.WhereFamDatabase
 import com.wherefam.android.data.ipc.IPCProvider
@@ -19,35 +24,31 @@ import org.koin.dsl.module
 
 val appModule = module {
     single { LocationManager(get()) }
-
-    single { IPCProvider.ipc }
-
+    single { IPCProvider.ipc!! }
     single { DataStoreRepository(get()) }
     single<UserRepository> { UserRepositoryImpl(get()) }
 
     single {
-        Room.databaseBuilder(
-            get(),
-            WhereFamDatabase::class.java,
-            "wherefam_database"
-        ).build()
+        Room.databaseBuilder(get(), WhereFamDatabase::class.java, "wherefam_database")
+            .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+            .build()
     }
 
-    single {
-        val db = get<WhereFamDatabase>()
-        db.peerRepository
-    }
+    single { get<WhereFamDatabase>().peerDao }
+    single { get<WhereFamDatabase>().placeDao }
+    single { get<WhereFamDatabase>().locationHistoryDao }
 
-    single { GenericMessageProcessor(get(), get()) }
+    single { GenericMessageProcessor(get(), get(), get(), get(), get(), get()) }
 }
 
 val viewModel = module {
     viewModel { SplashViewModel(get()) }
     viewModel { OnboardingViewModel(get()) }
     viewModel { ThirdPageViewModel(get(), get()) }
-
     viewModel { HomeViewModel(get(), get(), get()) }
     viewModel { ShareViewModel(get()) }
     viewModel { PeopleViewModel(get(), get()) }
-
+    viewModel { PlacesViewModel(get(), get()) }
+    viewModel { SafetyViewModel(get(), get()) }
+    viewModel { SettingsViewModel(get(), get()) }
 }
